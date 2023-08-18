27eae054f8d5a4db860ed4a6ac41795c00183022
Jakub Bartmiński <jakub@bartmin.ski>
2023-08-18 18:08:37 +0200

    s/maks/tree przedzial-przedzial.cpp

--- wwi23/przedzial-przedzial.cpp
+++ wwi23/przedzial-przedzial.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 const int n_max = 1024*1024;
 int lazy[2*n_max];
-int maks[2*n_max];
+int tree[2*n_max];
 
 // funkcja push powinna tylko "poprawiac" drzewa 
 // zawolanie wiele razy nie moze nic psuc
 void push(int akt)
 {
-    // 1. zaktualizowac "maks" uzywajac wartosci w "lazy"
-    maks[akt] += lazy[akt];
+    // 1. zaktualizowac "tree" uzywajac wartosci w "lazy"
+    tree[akt] += lazy[akt];
     
     // 2. zepchnac "lazy" do synow
     if (akt < n_max) // w lisciach nie spychamy (bo nie ma gdzie)
@@ -25,8 +25,8 @@ void push(int akt)
 }
 
 // dodaj wartosc na przedziale [zap_pocz, zap_kon]
-// implementacja analogiczna do punkt-przedzial, ale dodajemy do lazy, zamiast do maks
-// zawsze wolamy push na poczatku, bo musimy miec aktualne wartosci w tablicy "maks"
+// implementacja analogiczna do punkt-przedzial, ale dodajemy do lazy, zamiast do tree
+// zawsze wolamy push na poczatku, bo musimy miec aktualne wartosci w tablicy "tree"
 // (do aktualizacji rodzica przy wychodzeniu z trzeciej galezi ifa)
 void add(int zap_pocz, int zap_kon, int wartosc, int akt=1, int akt_pocz=0, int akt_kon=n_max-1)
 {
@@ -47,7 +47,7 @@ void add(int zap_pocz, int zap_kon, int wartosc, int akt=1, int akt_pocz=0, int 
         /* albo: wersja alternatywna bez dodawania do lazy[akt] i bez push (ale powtarza czesc kodu z push):
          * // pamietamy ze musimy zostawic lazy[akt] == 0 przed wyjsciem
          * // wszystkie leniwe zmiany musza byc w synach
-         * maks[akt] += wartosc;
+         * tree[akt] += wartosc;
          * if (akt < n_max)
          * {
          *    lazy[2*akt] += wartosc;
@@ -61,13 +61,13 @@ void add(int zap_pocz, int zap_kon, int wartosc, int akt=1, int akt_pocz=0, int 
         int akt_sro = (akt_pocz + akt_kon) / 2;
         add(zap_pocz, zap_kon, wartosc, 2*akt, akt_pocz, akt_sro); // lewy syn
         add(zap_pocz, zap_kon, wartosc, 2*akt + 1, akt_sro + 1, akt_kon); // prawy syn
-        maks[akt] = max(maks[2*akt], maks[2*akt + 1]); // aktualizujemy rodzica tak samo jak w punkt-przedzial
+        tree[akt] = max(tree[2*akt], tree[2*akt + 1]); // aktualizujemy rodzica tak samo jak w punkt-przedzial
     }
 }
 
 // znajdz maksimum na przedziale [zap_pocz, zap_kon]
 // implementacja tak samo jak w punkt-przedzial jedyna zmiana to 
-// zawsze wolamy push na poczatku, zeby miec aktualne wartosci w tablicy "maks"
+// zawsze wolamy push na poczatku, zeby miec aktualne wartosci w drzewie "tree"
 int get_maks(int zap_pocz, int zap_kon, int akt=1, int akt_pocz=0, int akt_kon=n_max-1)
 {
     push(akt); // zawsze push, bo jesli zostawimy lazy[akt] != 0, to chodzimy po nieaktualnych wartosciach
@@ -78,7 +78,7 @@ int get_maks(int zap_pocz, int zap_kon, int akt=1, int akt_pocz=0, int akt_kon=n
     }
     else if (zap_pocz <= akt_pocz && akt_kon <= zap_kon)
     {
-        return maks[akt];
+        return tree[akt];
     }
     else
     {
